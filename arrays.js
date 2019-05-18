420a373c59dac96cddc415478ee10275bcf7875b
Aude Faucheux <aude53@gmail.com>
2019-05-18 18:23:10 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,17 @@
+var chocolateBars = ['snickers','hundred grand','kitkat','skittles']
+
+function addElementToBeginningOfArray(array,element) {
+  var array = [1];
+  ['foo',...addElementToBeginningOfArray];
+}
+
+function destructivelyAddElementToBeginningOfArray(array,element) {
+}
+
+function addElementToEndOfArray(){
+
+}
+
+function destructivelyAddElementToEndOfArray() {
+
+}
